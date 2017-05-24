567ef906b383e146670f585a835b45cdbfe41f87
saylorsd <sds25@pitt.edu>
2017-05-24 16:16:26 -0400

    testing travis ci

--- header.php
+++ header.php
@@ -15,19 +15,27 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<?php wp_head(); ?>
+
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+              rel="stylesheet">
+
 	</head>
 
-	<body <?php body_class(); ?>>
+	<body <?php body_class(); ?> >
+
 	<?php do_action( 'foundationpress_after_body' ); ?>
 
 	<?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) === 'offcanvas' ) : ?>
+
 	<div class="off-canvas-wrapper">
 		<?php get_template_part( 'template-parts/mobile-off-canvas' ); ?>
 	<?php endif; ?>
 
 	<?php do_action( 'foundationpress_layout_start' ); ?>
 
+    <!-- Top Bar-->
 	<header class="site-header" role="banner">
+        <!-- Mobile -->
 		<div class="site-title-bar title-bar" <?php foundationpress_title_bar_responsive_toggle() ?>>
 			<div class="title-bar-left">
 				<button class="menu-icon" type="button" data-toggle="<?php foundationpress_mobile_menu_id(); ?>"></button>
@@ -36,10 +44,15 @@
 				</span>
 			</div>
 		</div>
+
+        <!-- Computer -->
 		<nav class="site-navigation top-bar" role="navigation">
 			<div class="top-bar-left">
 				<div class="site-desktop-title top-bar-title">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+                        <img class="site-title-logo" src="<?php echo get_template_directory_uri()?>/assets/images/plain_logo_rbg_bridge.svg"/>
+                    </a>
+                    <span class="site-title">Western Pennsylvania Regional Data Center</span>
 				</div>
 			</div>
 			<div class="top-bar-right">
