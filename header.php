5c1b204e49be84a27dbda5953a00c3938b6734fe
Steven Saylor <steven.saylor@pitt.edu>
2017-05-25 14:34:32 -0400

    Begin changes (#2)
    * testing travis ci
    
    * enqueued material-icon stylesheet
    
    * fixed strings

--- header.php
+++ header.php
@@ -17,17 +17,21 @@
 		<?php wp_head(); ?>
 	</head>
 
-	<body <?php body_class(); ?>>
+	<body <?php body_class(); ?> >
+
 	<?php do_action( 'foundationpress_after_body' ); ?>
 
 	<?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) === 'offcanvas' ) : ?>
+
 	<div class="off-canvas-wrapper">
 		<?php get_template_part( 'template-parts/mobile-off-canvas' ); ?>
 	<?php endif; ?>
 
 	<?php do_action( 'foundationpress_layout_start' ); ?>
 
+    <!-- Top Bar-->
 	<header class="site-header" role="banner">
+        <!-- Mobile -->
 		<div class="site-title-bar title-bar" <?php foundationpress_title_bar_responsive_toggle() ?>>
 			<div class="title-bar-left">
 				<button class="menu-icon" type="button" data-toggle="<?php foundationpress_mobile_menu_id(); ?>"></button>
@@ -36,10 +40,15 @@
 				</span>
 			</div>
 		</div>
+
+        <!-- Computer -->
 		<nav class="site-navigation top-bar" role="navigation">
 			<div class="top-bar-left">
 				<div class="site-desktop-title top-bar-title">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+                        <img class="site-title-logo" src="<?php echo get_template_directory_uri()?>/assets/images/plain_logo_rbg_bridge.svg"/>
+                    </a>
+                    <span class="site-title">Western Pennsylvania Regional Data Center</span>
 				</div>
 			</div>
 			<div class="top-bar-right">
